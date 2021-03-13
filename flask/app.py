d840a2e423b7a73bdcb09cce2361b3f102fb130a
RSciFi <f.geo@posteo.de>
2021-03-13 22:48:18 +0100

    compare to added

--- flask/app.py
+++ flask/app.py
@@ -2,8 +2,16 @@ from flask import Flask, request, jsonify, redirect
 from flask_cors import CORS
 
 import pandas as pd
+import numpy as np
 import datetime as dt
 import json
+
+def compare(co2eq,compareTo):
+    areafrac_arctic = 3700000/510000000
+    mass_global = 5.13*10**15
+    mass_arctic = mass_global*areafrac_arctic
+    tons = round((co2eq*1e-06*mass_arctic),3) # gt 1e-9
+    return round(tons/comps[compareTo],1)
 
 # complement time dims if not existent
 def complement(df):
@@ -23,15 +31,31 @@ def load_df(file):
     df = df[['time','value']].set_index('time')
     return df
 
-# load files into dictionary
-data_vars = ['carbon_dioxide','methane']
+metadata = pd.read_csv('metadata.csv',sep=';').set_index('index')
+data_vars = list(metadata.index)            # ['co2','ch4']
+
+# load files into dict
 data = {i: load_df(i) for i in data_vars}
 
-var = 'methane'
-df = data[var]
+# get time.minmax vals for metadata dict
+data_range = pd.DataFrame(
+    {i: {'minDate':data[i].index.min().strftime('%Y-%m-%d'),
+         'maxDate':data[i].index.max().strftime('%Y-%m-%d'),
+         'minYear':data[i].index.min().strftime('%Y'),
+         'maxYear':data[i].index.max().strftime('%Y')} for i in data_vars}).T
+metadata[['minDate','maxDate','minYear','maxYear']] = data_range
+metadata['compareTo'] = [['cows','cars'],['cows','cars']]
+metadata = metadata.to_dict('index')
 
+unit_conversion = {'ch4': 0.001,
+                   'co2': 1}
 
+comps = {'cows': 100*0.001*23,
+         'cars': 110*1e-6*12000}
 
+# select dataset
+# var = 'ch4'
+# df = data[var]
 
 # # read data
 # df = pd.read_csv('methane.csv', sep=';')
@@ -40,33 +64,43 @@ df = data[var]
 # df = df[['time','value']].set_index('time')
 
 def return_period(df, start_date,end_date):
-    begin,end = pd.Timestamp(start_date),pd.Timestamp(end_date)
-    mask = (df.index >= begin) & (df.index < end)
+    begin,end = pd.Timestamp(start_date),pd.Timestamp(str(int(end_date)+1))
+    mask = (df.index >= begin) & (df.index <= end)
     return df.loc[mask]
 
 def return_change(df):
-    return df['value'].loc[df.index.max()] - df['value'].loc[df.index.min()]
+    # dmin,dmax = df['value'].loc[df.index.min()],df['value'].loc[df.index.max()]
+    ymeans = [round(df.loc[df.index.year == y]['value'].mean(), 2) for y in np.unique(df.index.year)]
+    delta = ymeans[-1]-ymeans[0]
+    return round(delta,2), round(ymeans[0],2), round(ymeans[-1],2)
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/datasets/')
-def datasets()
-    return jsonify(response)
+def datasets():
+    return jsonify(metadata)
 
 @app.route('/datapoints/')
 def datapoints():
-    data = request.args.get('dataset')
+    data_key = request.args.get('dataset')
+    df = data[data_key]
     start_date = request.args.get('startdate')
     end_date = request.args.get('enddate')
-
     period = return_period(df,start_date,end_date)
-    change = return_change(period)
+    change,ymin,ymax = return_change(period)
     # return render_template('index.html')
-    response = {'dataset': data,
-                'begin_period': period.index.min().strftime("%Y-%m-%d"),
-                'end_period': period.index.max().strftime("%Y-%m-%d"),
-                'change': change}
+    x = round(change * unit_conversion[data_key],4)
+    co2eq = x*23 if data_key=='ch4' else x
+    response = {'dataset': data_key,
+                'begin_period': period.index.min().strftime("%Y"),
+                'end_period': period.index.max().strftime("%Y"),
+                'change': x,
+                'begin_data': round(ymin * unit_conversion[data_key],2),
+                'end_data': round(ymax * unit_conversion[data_key],2)}
+    compareTo = request.args.get('compareTo')
+    if compareTo:
+        response['comp_amount'] = compare(co2eq, compareTo)
     return jsonify(response)
 
 # @app.route('/study')
