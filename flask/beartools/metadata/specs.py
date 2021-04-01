ff787b978a9f1af5355069632d3acf18cc37aac9
geograFie <f.geo@posteo.de>
2021-04-01 11:35:13 +0200

    minor changes

--- flask/beartools/metadata/specs.py
+++ flask/beartools/metadata/specs.py
@@ -7,22 +7,40 @@ Parameter specifications
 
 param_specs = {
     'ch4': {'dataset': 'ICOS ATC NRT CH4 growing time series',
-            'column_label': 'ch4',
+            'label': 'ch4',
+            'variable': None,
+            'access_folders': None,
             'type': 'CH4 mixing ratio (dry mole fraction, ppb)',
             'unit': 'nmol mol-1',
             'unit_conv': 0.001,
+            'dst_unit': 'ppm',
             'category': 'gas',
             'co2eq_factor': 23,
             'round_decimals': 2},
 
     'co2': {'dataset': 'ICOS ATC NRT CO2 growing time series',
-            'column_label': 'co2',
+            'label': 'co2',
+            'variable': None,
+            'access_folders': None,
             'type': 'CO2 mixing ratio (dry mole fraction, ppm)',
             'unit': 'µmol mol-1',
             'unit_conv': 1,
+            'dst_unit': 'ppm',
             'category': 'gas',
             'co2eq_factor': 1,
-            'round_decimals': 2}
+            'round_decimals': 2},
+
+    'sic': {'dataset': 'ssmi-ssmis-hydrological-products',
+            'label': 'mw-hydro_v01_2.5-deg_ice_early',
+            'variable': 'sea_ice_cover',
+            'access_folders': 'access/netcdf/',
+            'type': None,
+            'unit': None,
+            'unit_conv': None,
+            'dst_unit': None,
+            'category': 'area',
+            'co2eq_factor': None,
+            'round_decimals': None}
 }
 
 class ParamSpecs:
@@ -31,13 +49,14 @@ class ParamSpecs:
         self.specs = param_specs
 
         self.dataset = self.specs[self.param]['dataset']
-        self.label = self.specs[self.param]['column_label']
+        self.label = self.specs[self.param]['label']
+        self.variable = self.specs[self.param]['variable']
+        self.access = self.specs[self.param]['access_folders']
         self.type = self.specs[self.param]['type']
         self.unit = self.specs[self.param]['unit']
         self.unit_conv = self.specs[self.param]['unit_conv']
         self.category = self.specs[self.param]['category']
         self.decimals = self.specs[self.param]['round_decimals']
         self.co2eq = self.specs[self.param]['co2eq_factor']
-
 
 
