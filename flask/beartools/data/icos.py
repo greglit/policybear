46c481176580af2cf2b1647ab957227105e8ffa3
geograFie <67632196+geograFie@users.noreply.github.com>
2021-05-28 21:25:27 +0200

    major performance improvements in ICOS metadata fetching

--- flask/beartools/data/icos.py
+++ flask/beartools/data/icos.py
@@ -15,28 +15,61 @@ import icoscp
 from icoscp.station import station
 from icoscp.cpb.dobj import Dobj
 
+# swap inspection
+from . import swap
+
 from ..metadata.specs import ParamSpecs
 
 class Fetch:
-    def __init__(self, ID, ParamSpecs):
+    def __init__(self, ID, ParamSpecs, data_swap):
         self.ID = ID
         self.PS = ParamSpecs
         self.param = self.PS.param
-        self.dataset = self.PS.dataset    # ["ICOS ATC CH4 Release", "ICOS ATC NRT CH4 growing time series"]
-        self.metadata = self.get_metadata()
+        self.dataset = self.PS.dataset  # ["ICOS ATC CH4 Release", "ICOS ATC NRT CH4 growing time series"]
+        self.data_swap = data_swap
+        self.data_swap_status = self.check_data_swap_status()
 
-    def get_metadata(self):
+        if not all(self.data_swap_status.values()):
+            self.metadata = self.fetch_station_metadata()
+
+    def header_data_swap(self):
         """
+        return simple data swap header without the data.
         """
+        header = {
+            station: list(data.columns)
+            for station, data in self.data_swap.items()
+        }
+        return header
+
+    def check_data_swap_status(self):
+        """
+        Check if data already loaded into swap storage.
+        """
+        print('ICOS: Fetch | data swap status | ... checking data availability in data swap storage ...')
+        data_swapped = self.header_data_swap()
+
+        if (self.ID not in data_swapped):
+            station_status, param_status = False, False
+        elif (self.param not in data_swapped[self.ID]):
+            station_status, param_status = True, False
+        else:
+            station_status, param_status = True, True
+        print(f'ICOS: Fetch | data swap status | {self.ID}: {station_status},  {self.param}: {param_status}')
+        return {'station': station_status, 'param': param_status}
+
+    def fetch_station_metadata(self):
+        print('ICOS: Fetch | station metadata | ... fetching data infos, including dobj (download link) ...')
+
         def preprocessing(meta):
             # make sure samplingheight type is float (otherwise object)
             meta['samplingheight'] = meta['samplingheight'].astype(float, errors='ignore')
             return meta
-        print('debug: fetching metadata')
 
         meta = station.get(self.ID).data()
         meta = meta.loc[meta.specLabel.isin(self.dataset)]
         meta = preprocessing(meta)
+        print(f'ICOS: Fetch | station metadata | Fetching metadata for {self.ID} successful.')
         return meta
 
     def list_heights(self):
@@ -46,7 +79,7 @@ class Fetch:
         height = self.list_heights()[0]
         return self.metadata.loc[self.metadata['samplingheight'] == height]
 
-    def fetch_data(self, dobj):
+    def get_data(self, dobj):
         data = Dobj(dobj).data
         data = data[['TIMESTAMP', self.param]]
         return data.set_index('TIMESTAMP')  # ??? [['ch4']]
@@ -55,30 +88,37 @@ class Fetch:
         """input as pd.Series"""
         return ds * self.PS.unit_conv
 
-    def get_data(self):
+    def fetch_data(self):
+        print(f'ICOS: Fetch | data | ... fetching data ...')
         meta = self.select_height()
         dobjs = list(meta['dobj'])
         data = pd.concat(
-            [self.fetch_data(dobj) for dobj in dobjs],
+            [self.get_data(dobj) for dobj in dobjs],
             axis=0,
             # keys=['ATC','ATC NRT'] # hierarchical index
         ) # check overlapping with verify_integrity, will return exception!!
         data.loc[:,self.param] = self.unit_conv(data.loc[:,self.param])
         return data[[self.param]]
 
-    def collect_data(self, dct):
-        if (self.ID not in dct):
-            print('Fetching data for: {} and {}'.format(self.ID,self.param))
-            data = self.get_data()
-            dct[self.ID] = data
+    def fetch_and_swap_data(self, data_swap):
+        if all(self.data_swap_status.values()):
+            print('ICOS: Fetch | data | Skip fetching, data already swapped.')
 
-        elif self.param not in dct[self.ID].columns:
-            print('Station {} loaded, fetching data for different parameter: {}'.format(self.ID,self.param))
-            data = self.get_data()
-            dct[self.ID] = pd.concat([dct[self.ID], data], axis=1)
+        elif not self.data_swap_status['station']:
+            data = self.fetch_data()
+            print(f'ICOS: Fetch | data | swaping data, creating entry:  {self.ID} with {self.param}')
+            # self.data_swap[self.ID] = data
+            data_swap[self.ID] = data
 
-        else:
-            print('Skip fetching data, already loaded for {} and {}'.format(self.ID,self.param))
+        elif not self.data_swap_status['param']:
+            data = self.fetch_data()
+            print(f'ICOS: Fetch | data | swaping data, creating entry:  ({self.ID}) with {self.param}')
+            # self.data_swap[self.ID] = pd.concat([self.data_swap[self.ID], data], axis=1)
+            data_swap[self.ID] = pd.concat([data_swap[self.ID], data], axis=1)
+
+        # Check size of data swap if new data was fetched
+        if not all(self.data_swap_status.values()):
+            swap.inspect_data_swap(data_swap)
 
 
 # END OF FILE
