77a7b7063abb69cfcab92723a4b6e73283834972
geograFie <67632196+geograFie@users.noreply.github.com>
2021-05-30 20:02:16 +0200

    added mongodb service

--- flask/mongodb.py
+++ flask/mongodb.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
 """
-Created Mar 12, 2021
+Created May 30, 2021
 
 Connect to MongoDB server.
 """
 
 import os
-def index():
-    times = os.environ.get('LSCOLORS')
-    return times
+import pymongo
+from pymongo import MongoClient
 
-index()
+#%%
+# MongoDB access URI
+mongodb_uri = os.environ.get('MONGODB_URI')
+client = MongoClient(mongodb_uri)
+
+db = client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult = db.command("serverStatus")
+
+
+# # creating new database "testing"
+# db = client.testing
+#
+# sample_data = {
+#     'stations': {
+#         'name': 'KIT',
+#         'latlon': [50.12,10.02]
+#     },
+#     'random': [2,4,6,7,8,9,9]
+# }
+#
+# result = db.review
+#
+# def index():
+#     times = os.environ.get('LSCOLORS')
+#     return times
+#
+# index()
 
 
 #%%
